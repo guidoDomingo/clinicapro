b47562c66971ca7f9ffde145a643b602ed764f8c
Guido Ruiz <ruizbenitezguido11@gmail.com>
2025-06-25 00:17:46 -0300

    actualizado 14.10.7 (limpieza de archivos inecesarios)

--- fix_js.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Fixing reservas_new.js file...
-copy "c:\laragon\www\clinica\view\js\reservas_new.js" "c:\laragon\www\clinica\view\js\reservas_new.js.backup_%date:~-4,4%%date:~-7,2%%date:~-10,2%" /Y
-copy "c:\laragon\www\clinica\view\js\reservas_new.js.fixed" "c:\laragon\www\clinica\view\js\reservas_new.js" /Y
-echo Fixed successfully!
-pause
