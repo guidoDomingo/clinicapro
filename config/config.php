78435ec110ae191cb2a8475263162d48a11b8452
antoniogaleano <galeanoantonio6@gmail.com>
2025-07-30 19:47:24 -0300

    Actualización de conexion.php

--- config/config.php
+++ config/config.php
@@ -11,11 +11,11 @@ require_once __DIR__ . '/../vendor/autoload.php';
 // Database configuration
 $dbConfig = [
     'driver' => 'pgsql',
-    'host' => 'localhost',
-    'port' => '5432',
+    'host' => '181.122.125.143',
+    'port' => '5454',
     'database' => 'clinica',
-    'username' => 'postgres',
-    'password' => 'admin'
+    'username' => 'acmeuser',
+    'password' => 'wjstks'
 ];
 
 // Initialize the database connection
